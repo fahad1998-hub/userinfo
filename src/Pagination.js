ca9a6841b70b3909be002ff3b9c3458e1e423c23
fahad_since1998 <ashraffahad498@gmail.com>
2021-09-25 11:16:47 +0530

    second page render

--- src/Pagination.js
+++ src/Pagination.js
@@ -3,14 +3,14 @@ import Pagination from '@mui/material/Pagination';
 import Stack from '@mui/material/Stack';
 
 export default function PaginationControlled({ page, setPage, userData }) {
-    const handleChange = (value) => {
+    const handleChange = ( event, value) => {
         setPage(value);
     };
 
     return (
         <Stack spacing={2}>
             <Pagination count={userData.total_pages} page={page} onChange={handleChange}
-                style={{ display: "flex", justifyContent: "center", marginTop: '2rem' }} />
+                style={{ display: "flex", justifyContent: "center", marginTop: '3rem' }} />
         </Stack>
     );
 }
